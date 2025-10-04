2be68aab0fcdfdf2245c6a0a8f9c425337bddd4f
yiqingpeng <345095194@qq.com>
2025-10-04 13:56:48 +0800

    filecxx-AC-parser.js

---
+++ res/filecxx-AC-parser.js
@@ -0,0 +1,21 @@
+function filecxxACParser(html, datetime) {
+	var regex = /<pre[^>]*id="codes"[^>]*>([\s\S]*?)<\/pre>/i;
+    var matches = html.match(regex);
+	var activeCode = "";
+	if (matches && matches[1]) {
+		var dateRangeRegex = /(\d{4}-\d{2}-\d{2}) 00:00:00 - (\d{4}-\d{2}-\d{2}) 00:00:00/;
+		var lines = matches[1].split("\n");
+		for (var i = 0; i < lines.length; i++) {
+			var line = lines[i].replace(/^\s+|\s+$/g,"");
+			if (!line) continue;
+			var dates = line.match(dateRangeRegex);
+			if (!dates) continue;
+			
+			if (datetime >= dates[1] && datetime < dates[2]) {
+				activeCode = lines[i+1];
+				break;
+			}
+		}
+	}
+	return {"activeCode":activeCode.replace(/^\s+|\s+$/g,""), "winTitle": "文件蜈蚣 - 激活码", "editorType": "可编辑文本", "buttonType": "按下按钮", "buttonName": "确定", "acLength": 150, "autoSubmit": 0, "tipMsg": "请打开激活码界面"};
+}
